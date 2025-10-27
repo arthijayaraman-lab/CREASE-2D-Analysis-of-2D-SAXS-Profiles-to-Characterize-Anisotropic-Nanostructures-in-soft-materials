27741f653b5ea29bf50985ccdf7267462c35bda5
Sri Vishnuvardhan Reddy Akepati <60961118+sri-vishnu-001@users.noreply.github.com>
2025-10-27 15:38:27 -0400

    Disable raw plot generation in example2Dplots.m
    Comment out the call to generaterawplot in the loop.

--- Experimental_data/process_experimental_data/example2Dplots.m
+++ Experimental_data/process_experimental_data/example2Dplots.m
@@ -5,6 +5,7 @@ for i=6%1:numfiles
     filename=filenames{i};
     samplename=filename(1:end-18);
     generate2Dscatteringplot(samplename,'./','./',0);
-    generaterawplot(samplename,'./','./');
+    %generaterawplot(samplename,'./','./');
 end
+
 
